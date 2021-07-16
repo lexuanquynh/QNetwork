5d4d2825a9efc1eb539ad088bb083d40c0149615
Le Xuan Quynh <le-xuan-quynh@goodcycle.net>
2021-07-16 09:39:43 +0700

    able change baseUrl

--- Sources/QNetwork/Configs.swift
+++ Sources/QNetwork/Configs.swift
@@ -10,11 +10,13 @@ import Foundation
 public struct Configs {
     public struct Network {
         // set true for tests and generating screenshots with fastlane
-        public static let useStaging = false
+        public static var useStaging = false
         #if DEBUG
-        public static let baseUrl = "https://api.github.com/"
+        // Link for development
+        public static var baseUrl = "https://api.github.com/"
         #else
-        public static let baseUrl = "https://api.github.com/"
+        // Link for production
+        public static var baseUrl = "https://api.github.com/"
         #endif
     }
 }
